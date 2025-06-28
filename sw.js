2d822931ce66ee3fd06872437754dcced93bd764
Atomregen <32017240+Atomregen@users.noreply.github.com>
2025-06-28 09:14:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Dieser Service Worker ist für die PWA-Funktionalität notwendig.
+// Er muss nicht viel tun, nur existieren und sich selbst registrieren.
+self.addEventListener('fetch', event => {
+  // Wir übergeben alle Netzwerkanfragen einfach weiter.
+});
