e87da2dab44a0a57b83cca909b48f292b004d347
Atomregen <32017240+Atomregen@users.noreply.github.com>
2025-06-28 13:12:12 +0200

    Delete docs/sw.js

--- docs/sw.js
+++
@@ -1,5 +0,0 @@
-// This service worker is necessary for PWA functionality.
-// It doesn't have to do much, just exist and register itself.
-self.addEventListener('fetch', event => {
-  // We just pass all network requests on.
-});
